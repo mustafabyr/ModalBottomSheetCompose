1218305877e6a2457641e57c7c538f8c2638e1a6
MUSTAFA BAYAR <mustafa.bayar.132@gmail.com>
2023-08-25 10:17:31 +0300

    fullscreen

--- app/src/main/java/com/mbyrapps/bottomsheetcompose/MainActivity.kt
+++ app/src/main/java/com/mbyrapps/bottomsheetcompose/MainActivity.kt
@@ -3,19 +3,32 @@ package com.mbyrapps.bottomsheetcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -34,17 +47,55 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     //implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-                    val sheetScaffoldState = rememberBottomSheetScaffoldState()
+                    val sheetScaffoldState = rememberBottomSheetScaffoldState(
+                        bottomSheetState = SheetState(
+                            skipPartiallyExpanded = true,
+                            skipHiddenState = false,
+                            initialValue = SheetValue.Hidden
+                        )
+                    )
                     val scope = rememberCoroutineScope()
+                    var isSheetFullScreen by remember {
+                        mutableStateOf(false)
+                    }
                     BottomSheetScaffold(
-
                         scaffoldState = sheetScaffoldState,
                         sheetContent = {
                             ModalBottomSheet(
-                                modifier = Modifier.padding(12.dp)
-                            )
+                                modifier =
+                                if (isSheetFullScreen) {
+                                    Modifier
+                                        .fillMaxSize()
+                                        .padding(12.dp)
+                                } else {
+                                    Modifier
+                                        .padding(12.dp)
+                                }
+
+                            ) {
+                                isSheetFullScreen = true
+                            }
                         },
-                        sheetPeekHeight = 0.dp
+                        sheetPeekHeight = 0.dp,
+                        sheetSwipeEnabled = false,
+                        sheetDragHandle = {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.End
+                            ) {
+                                IconButton(onClick = {
+                                    scope.launch {
+                                        sheetScaffoldState.bottomSheetState.hide()
+                                        isSheetFullScreen = false
+                                    }
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.Default.Close,
+                                        contentDescription = null
+                                    )
+                                }
+                            }
+                        }
                     ) {
                         Box(
                             modifier = Modifier.fillMaxSize(),
@@ -68,12 +119,18 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun ModalBottomSheet(modifier: Modifier = Modifier) {
+fun ModalBottomSheet(modifier: Modifier = Modifier, onClickFullScreen: () -> Unit) {
     Column(
         modifier = modifier
     ) {
         for (i in 0..6) {
             Text(text = "Item $i")
+        }
+        Button(onClick = {
+            onClickFullScreen()
+        }
+        ) {
+            Text(text = "Open Sheet")
         }
     }
 }
